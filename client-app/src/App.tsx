7de3755e4fee5859647a52b239e062401ffb2a81
lan <tanlanhcmus@gmail.com>
2021-08-12 23:31:10 +0700

    feat(): add crud activity

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -5,11 +5,11 @@ import { Header, List } from 'semantic-ui-react';
 import { Activity } from './shared/models/activity';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get('https://localhost:5001/api/activities');
+        const response = await axios.get<Activity[]>('https://localhost:5001/api/activities');
         setActivities(response.data);
       } catch (error) {
         console.log(error);
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <div>
-      <Header as='h2' icon='users' content='Reactivities' />      
+      <Header as='h2' icon='users' content='Reactivities'></Header>  
         <List>
           {activities.map((activity: Activity) => (
             <List.Item key={activity.id}>
